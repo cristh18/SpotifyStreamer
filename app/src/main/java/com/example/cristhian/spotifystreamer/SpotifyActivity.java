3eb973ece091477cb0c8182b1e94fefea4353496
cristh18 <dar_funeral18@hotmail.com>
2015-07-09 23:26:34 -0500

    UpdateProject

--- app/src/main/java/com/example/cristhian/spotifystreamer/SpotifyActivity.java
+++ app/src/main/java/com/example/cristhian/spotifystreamer/SpotifyActivity.java
@@ -3,16 +3,40 @@ package com.example.cristhian.spotifystreamer;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /**
  * Created by Cristhian on 6/24/2015.
  */
-public class SpotifyActivity extends Activity {
+public class SpotifyActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_spotify);
         getFragmentManager().beginTransaction().add(R.id.spoti_container, new SpotifyFragment()).commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_spotify, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
